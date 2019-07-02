0d699062f771c23452ac801de72d2c364766c942
Dalton Spillman <dalton_spillman@yahoo.com>
2019-07-01 21:20:28 -0500

    Created Android and UWP projects

---
+++ SkiaSharp.UwpSnapshotBug/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaSharp.UwpSnapshotBug
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPaintSurface(object sender, SKPaintGLSurfaceEventArgs e)
+        {
+            var canvas = e.Surface.Canvas;
+
+            canvas.Clear(SKColors.White);
+            canvas.Flush();
+
+            using (var snapshotImage = e.Surface.Snapshot())
+            using (var bitmap = SKBitmap.FromImage(snapshotImage))
+            {
+                Debug.WriteLine($"Has non-zero color: {bitmap.Pixels.Any(pixel => pixel != new SKColor(0, 0, 0, 0))}");
+            }
+        }
+    }
+}
